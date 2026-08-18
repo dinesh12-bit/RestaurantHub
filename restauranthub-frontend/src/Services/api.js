780446b3850edf5163abf852a85ca3ef3787d001
dinesh12-bit <dineshsahuji8168@gmail.com>
2026-08-18 21:50:19 +0530

    Update production backend API URL

--- restauranthub-frontend/src/Services/api.js
+++ restauranthub-frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://restauranthub-production-4dfe.up.railway.app/api",
     headers: {
         "Content-Type": "application/json"
     }
